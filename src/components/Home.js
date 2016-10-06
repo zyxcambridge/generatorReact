7fd9855170af036e716baf4eea46335fade7cdf7
cambridge <cambridge@cambridgedeMacBook-Pro.local>
2016-10-07 02:52:14 +0800

    bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,30 +5,97 @@ import ReactDOM from 'react-dom';
 import { Router, Route, Link } from 'react-router';
 import TopNavBar from "./TopNavBar.js"
 import Links from "./Links.js"
+import { Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+import { Popover } from 'react-bootstrap';
+import { Tooltip } from 'react-bootstrap';
+import { OverlayTrigger } from 'react-bootstrap';
+
+
+
+
+import { ButtonToolbar } from 'react-bootstrap';
+var Alert = require('react-bootstrap').Alert;
 
 const splashImg = require('../images/ui/general/bg_01.png');
 
-const Home = () => (
 
+const Home = React.createClass({
+  getInitialState() {
+    return { showModal: false };
+  },
 
-<div>
-<h1>Home</h1>
+  close() {
+    this.setState({ showModal: false });
+  },
 
-<div id="headerContent">
-  <img src = {splashImg}
-  alt="背景图"/>
+  open() {
+    this.setState({ showModal: true });
+  },
 
-  <p>我们非常重视你提交的每一项功能建议，让我们一起把网站建设得更好！</p>
+  render() {
+    const popover = (
+      <Popover id="modal-popover" title="popover">
+        very popover. such engagement
+      </Popover>
+    );
+    const tooltip = (
+      <Tooltip id="modal-tooltip">
+        wow.
+      </Tooltip>
+    );
 
-</div>
+    return (
+      <div>
+        <p>Click to get the full Modal experience!</p>
 
-<nav className="navbarzyx">
- <Links  />
-</nav>
+        <Button
+          bsStyle="primary"
+          bsSize="large"
+          onClick={this.open}
+        >
+          Launch demo modal
+        </Button>
 
-</div>
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Text in a modal</h4>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
 
+            <h4>Popover in a modal</h4>
+            <p>there is a <OverlayTrigger overlay={popover}><a href="#">popover</a></OverlayTrigger> here</p>
 
-)
+            <h4>Tooltips in a modal</h4>
+            <p>there is a <OverlayTrigger overlay={tooltip}><a href="#">tooltip</a></OverlayTrigger> here</p>
+
+            <hr />
+
+            <h4>Overflowing text to show scroll behavior</h4>
+            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+
+		<nav className="navbarzyx">
+		<Links  />
+		</nav>
+
+      </div>
+    );
+  }
+});
 
 export default Home;
